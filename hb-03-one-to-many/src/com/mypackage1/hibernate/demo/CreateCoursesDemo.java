18d3d7b36a95e952ac993cbff39bfc617cb828e6
Coditas <Coditas@DESKTOP-GEVL235>
2021-08-18 17:27:54 +0530

    Hibernate Advanced Mappings: OneToMany- Bi-directional- Adding entry in new table (Create courses for instructor)

---
+++ hb-03-one-to-many/src/com/mypackage1/hibernate/demo/CreateCoursesDemo.java
@@ -0,0 +1,69 @@
+package com.mypackage1.hibernate.demo;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import com.mypackage1.hibernate.demo.entity.Course;
+import com.mypackage1.hibernate.demo.entity.Instructor;
+import com.mypackage1.hibernate.demo.entity.InstructorDetail;
+import com.mypackage1.hibernate.demo.entity.Student;
+
+public class CreateCoursesDemo {
+
+	public static void main(String[] args) {
+
+		SessionFactory factory = new Configuration()
+								.configure("hibernate.cfg.xml")
+								.addAnnotatedClass(Instructor.class)
+								.addAnnotatedClass(InstructorDetail.class)
+								.addAnnotatedClass(Course.class)
+								.buildSessionFactory();
+	
+		// .addAnnotatedClass() takes an argument of class in which we have our
+		// Entity and Table Annotations.(i.e. Instructor class, InstructorDetail 
+		// class and also Course class).
+		
+		
+		Session session = factory.getCurrentSession();
+		
+		
+		try {
+			/// CREATING NEW COURSES AND ADDING THEM.
+			
+			// start a transaction
+			session.beginTransaction();
+
+			
+			// get the instructor from database
+			int theId = 1;
+			Instructor tempInstructor = session.get(Instructor.class, theId);
+			
+			// create some courses
+			Course tempCourse1 = new Course("Air Guitar - The Ultimate Guide");
+			Course tempCourse2 = new Course("The Pinball Masterclass");
+			
+			// add courses to instructor
+			tempInstructor.add(tempCourse1);
+			tempInstructor.add(tempCourse2);
+			
+			// save the courses
+			session.save(tempCourse1);
+			session.save(tempCourse2);
+			
+
+			// commit a transaction
+			session.getTransaction().commit();
+			
+			System.out.println("Done!");
+			
+		} finally {
+			// add clean up code
+			session.close();
+			
+			// close the factory
+			factory.close();
+		}
+	}
+
+}
