dbcfb94d45116c862aee4a708c76242e1b00a620
Coditas <Coditas@DESKTOP-GEVL235>
2021-08-18 17:07:15 +0530

    Hibernate Advanced Mappings: OneToMany- Bi-directional- Setting up new Course class and bi-directional relationship

---
+++ hb-03-one-to-many/src/com/mypackage1/jdbc/TestJdbc.java
@@ -0,0 +1,27 @@
+package com.mypackage1.jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class TestJdbc {
+// This file is for checking our jdbc connection.
+	public static void main(String[] args) {
+		
+		String jdbcUrl = "jdbc:mysql://localhost:3306/hb-01-one-to-one-uni?useSSL=false&serverTimezone=UTC";
+		String user = "hbstudent";
+		String pass = "hbstudent";
+
+		try {
+			
+			System.out.println("Connecting to database: " + jdbcUrl);
+			
+			Connection myConn = DriverManager.getConnection(jdbcUrl, user, pass);
+			
+			System.out.println("Connection successfull!");
+					
+		} catch(Exception exc) {
+			exc.printStackTrace();
+		}
+	}
+
+}
