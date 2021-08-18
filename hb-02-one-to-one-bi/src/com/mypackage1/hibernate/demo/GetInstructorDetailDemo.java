14eb686f88f1f21ca0272a41f25a961cb6b2ab0a
Coditas <Coditas@DESKTOP-GEVL235>
2021-08-18 14:40:04 +0530

    Hibernate Advanced Mappings: OneToOne- Bi-directional- Handling Exception and connection leak issue

--- hb-02-one-to-one-bi/src/com/mypackage1/hibernate/demo/GetInstructorDetailDemo.java
+++ hb-02-one-to-one-bi/src/com/mypackage1/hibernate/demo/GetInstructorDetailDemo.java
@@ -32,20 +32,31 @@ public class GetInstructorDetailDemo {
 			// get the instructor detail object
 			int theId = 2;
 			InstructorDetail tempInstructorDetail = session.get(InstructorDetail.class, theId);
+			// This will return null if theId is not valid (if entry is not present in database).
+			// Then our tempInstructorDetail will be null.
 			
 			// print the instructor detail
 			System.out.println("tempInstructorDetail: " + tempInstructorDetail);
 			
 			// print the ASSOCIATED INSTRUCTOR
 			System.out.println("the associated instructor: " + tempInstructorDetail.getInstructor());
-			
+			// If tempInstructorDetail is null, it will throw NullPointerException.
+			// And also it will show that our connection is leak because we don't close the session.
+			// So we catch that exception in catch block below.
+			// And we will also close the session in finally block to handle connection leak issue.
 
 			// commit a transaction
 			session.getTransaction().commit();
 			
 			System.out.println("Done!");
 			
-		} finally {
+		} catch (Exception exc) {
+			exc.printStackTrace();
+		} 
+		finally {
+			// handle connection leak issue
+			session.close();
+			
 			// close the factory
 			factory.close();
 		}
