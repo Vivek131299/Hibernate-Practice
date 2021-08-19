959c7443374dde6697057429f6ccde155ae490da
Coditas <Coditas@DESKTOP-GEVL235>
2021-08-19 15:46:54 +0530

    Hibernate Advanced Mappings: ManyToMany - Getting Courses for Student

--- hb-05-many-to-many/src/com/mypackage1/hibernate/demo/GetCoursesForMaryDemo.java
+++ hb-05-many-to-many/src/com/mypackage1/hibernate/demo/GetCoursesForMaryDemo.java
@@ -10,7 +10,7 @@ import com.mypackage1.hibernate.demo.entity.InstructorDetail;
 import com.mypackage1.hibernate.demo.entity.Review;
 import com.mypackage1.hibernate.demo.entity.Student;
 
-public class AddCoursesForMaryDemo {
+public class GetCoursesForMaryDemo {
 
 	public static void main(String[] args) {
 
@@ -32,7 +32,7 @@ public class AddCoursesForMaryDemo {
 		
 		
 		try {
-			/// GETTING COURSES FOR STUDENT.
+			/// CREATING NEW COURSES FOR STUDENT AND ADDING THEM.
 			
 			// start a transaction
 			session.beginTransaction();
@@ -44,6 +44,19 @@ public class AddCoursesForMaryDemo {
 			
 			System.out.println("\nLoaded student: " + tempStudent);
 			System.out.println("Courses: " + tempStudent.getCourses());
+			
+			// create more courses
+			Course tempCourse1 = new Course("Rubik's Cube - How to Speed Cube");
+			Course tempCourse2 = new Course("Atari 2600 - Game Development");
+			
+			// add mary to those courses
+			tempCourse1.addStudent(tempStudent);
+			tempCourse2.addStudent(tempStudent);
+			
+			// save the courses
+			System.out.println("\nSaving the courses...");
+			session.save(tempCourse1);
+			session.save(tempCourse2);
 			
 			
 
